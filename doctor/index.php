410f1f50e21e8b7b09d125b49544d774242d81e0
Kavinkumar B <97976387+Theternos@users.noreply.github.com>
2023-09-21 21:28:44 +0530

    doctor dashboard updated

--- doctor/index.php
+++ doctor/index.php
@@ -12,12 +12,112 @@
     <?php include("../patient/config.php") ?>
     <title>Dashboard</title>
     <style>
+        /* Grid-related CSS */
+
+        :root {
+            --square-size: 14px;
+            --square-gap: 5px;
+            --week-width: calc(var(--square-size) + var(--square-gap));
+        }
+
+        .months {
+            grid-area: months;
+        }
+
+        .days {
+            grid-area: days;
+        }
+
+        .squares {
+            grid-area: squares;
+        }
+
+        .graph {
+            display: inline-grid;
+            grid-template-areas: "empty months"
+                "days squares";
+            grid-template-columns: auto 1fr;
+            grid-gap: 5px;
+        }
+
+        .months {
+            display: grid;
+            grid-template-columns: calc(var(--week-width) * 4)
+                /* Jan */
+                calc(var(--week-width) * 4)
+                /* Feb */
+                calc(var(--week-width) * 4)
+                /* Mar */
+                calc(var(--week-width) * 5)
+                /* Apr */
+                calc(var(--week-width) * 4)
+                /* May */
+                calc(var(--week-width) * 4)
+                /* Jun */
+                calc(var(--week-width) * 5)
+                /* Jul */
+                calc(var(--week-width) * 4)
+                /* Aug */
+                calc(var(--week-width) * 4)
+                /* Sep */
+                calc(var(--week-width) * 5)
+                /* Oct */
+                calc(var(--week-width) * 4)
+                /* Nov */
+                calc(var(--week-width) * 5)
+                /* Dec */
+            ;
+        }
+
+        .days,
+        .squares {
+            display: grid;
+            grid-gap: var(--square-gap);
+            grid-template-rows: repeat(7, var(--square-size));
+        }
+
+        .squares {
+            grid-auto-flow: column;
+            grid-auto-columns: var(--square-size);
+        }
+
+        .graph {
+            padding: 20px;
+            border: 1px #e1e4e8 solid;
+            margin: 0 0 0 20px;
+        }
+
+        .days li:nth-child(odd) {
+            visibility: hidden;
+        }
+
+        .squares li {
+            background-color: #ebedf0;
+        }
+
+        .squares li[data-level="1"] {
+            background-color: #8ab8e3;
+        }
+
+        .squares li[data-level="2"] {
+            background-color: #3488d6;
+        }
+
+        .squares li[data-level="3"] {
+            background-color: #003668;
+        }
+
+        li {
+            list-style: none;
+        }
+
         .dashbord-tables,
         .doctor-heade {
             animation: transitionIn-Y-over 0.5s;
         }
 
-        .filter-container {
+        .filter-container,
+        .graph {
             animation: transitionIn-Y-bottom 0.5s;
         }
 
@@ -177,97 +277,25 @@
                 </tr>
                 <tr>
                     <td colspan="4">
-
-                        <center>
-                            <table class="filter-container doctor-header" style="border: none;width:95%" border="0">
-                                <tr>
-                                    <td>
-                                        <h3>Welcome!</h3>
-                                        <h1><?php echo $username  ?>.</h1>
-                                        <p>Thanks for joinnig with us. We are always trying to get you a complete service<br>
-                                            You can view your dailly schedule, Reach Patients Appointment at home!<br><br>
-                                        </p>
-                                        <a href="appointment.php" class="non-style-link"><button class="btn-primary btn" style="width:30%">View My Appointments</button></a>
-                                        <br>
-                                        <br>
-                                    </td>
-                                </tr>
-                            </table>
-                        </center>
-
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="4">
                         <table border="0" width="100%"">
                             <tr>
                                 <td width=" 50%">
 
                             <center>
-                                <table class="filter-container" style="border: none;" border="0">
+                                <table class="filter-container doctor-header" style="border: none;width:95%; margin-left: 5%;" border="0">
                                     <tr>
-                                        <td colspan="4">
-                                            <p style="font-size: 20px;font-weight:600;padding-left: 12px;">Today's Status</p>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td style="width: 25%;">
-                                            <div class="dashboard-items" style="padding:20px;margin:auto;width:95%;display: flex">
-                                                <div>
-                                                    <div class="h1-dashboard">
-                                                        <?php echo $doctorrow->num_rows  ?>
-                                                    </div><br>
-                                                    <div class="h3-dashboard" style="font-size: 17px">
-                                                        All Doctors &nbsp;&nbsp;
-                                                    </div>
-                                                </div>
-                                                <div class="btn-icon-back dashboard-icons" style="background-image: url('../img/icons/doctors-hover.svg');"></div>
+                                        <td>
+                                            <div style="max-height: 35vh;">
+                                                <h3>Welcome!</h3>
+                                                <h1><?php echo $username  ?>.</h1>
+                                                <p> We are always trying to get you a complete service<br>
+                                                    You can view your dailly schedule, Reach Patients Appointment at home!<br><br>
+                                                </p>
+                                                <a href="appointment.php" class="non-style-link"><button class="btn-primary btn" style="width:45%">View My Appointments</button></a>
+                                                <br>
+                                                <br>
                                             </div>
                                         </td>
-                                        <td style="width: 25%;">
-                                            <div class="dashboard-items" style="padding:20px;margin:auto;width:95%;display: flex;">
-                                                <div>
-                                                    <div class="h1-dashboard">
-                                                        <?php echo $patientrow->num_rows  ?>
-                                                    </div><br>
-                                                    <div class="h3-dashboard" style="font-size: 17px">
-                                                        All Patients &nbsp;&nbsp;
-                                                    </div>
-                                                </div>
-                                                <div class="btn-icon-back dashboard-icons" style="background-image: url('../img/icons/patients-hover.svg');"></div>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td style="width: 25%;">
-                                            <div class="dashboard-items" style="padding:20px;margin:auto;width:95%;display: flex; ">
-                                                <div>
-                                                    <div class="h1-dashboard">
-                                                        <?php echo $appointmentrow->num_rows  ?>
-                                                    </div><br>
-                                                    <div class="h3-dashboard" style="font-size: 17px">
-                                                        Today's Booking &nbsp;
-                                                    </div>
-                                                </div>
-                                                <div class="btn-icon-back dashboard-icons" style="margin-left: 0px;background-image: url('../img/icons/book-hover.svg');"></div>
-                                            </div>
-
-                                        </td>
-
-                                        <td style="width: 25%;">
-                                            <div class="dashboard-items" style="padding:20px;margin:auto;width:95%;display: flex;padding-top:21px;padding-bottom:21px;">
-                                                <div>
-                                                    <div class="h1-dashboard">
-                                                        <?php echo $schedulerow->num_rows  ?>
-                                                    </div><br>
-                                                    <div class="h3-dashboard" style="font-size: 17px">
-                                                        My Sessions
-                                                    </div>
-                                                </div>
-                                                <div class="btn-icon-back dashboard-icons" style="background-image: url('../img/icons/session-iceblue.svg');"></div>
-                                            </div>
-                                        </td>
-
                                     </tr>
                                 </table>
                             </center>
@@ -305,7 +333,7 @@
 
                                         <?php
                                         $nextweek = date("Y-m-d", strtotime("+1 week"));
-                                        $sqlmain = "select schedule.scheduleid,schedule.title,doctor.docname,schedule.scheduledate,schedule.scheduletime,schedule.nop from schedule inner join doctor on schedule.docid=doctor.docid  where schedule.scheduledate>='$today' and schedule.scheduledate<='$nextweek' order by schedule.scheduledate ASC";
+                                        $sqlmain = "SELECT schedule.scheduleid,schedule.title,doctor.docname,schedule.scheduledate,schedule.scheduletime,schedule.nop from schedule inner join doctor on schedule.docid=doctor.docid  where schedule.scheduledate>='$today' and schedule.scheduledate<='$nextweek' and schedule.docid = '$userid' order by schedule.scheduledate ASC";
                                         $result = $database->query($sqlmain);
 
                                         if ($result->num_rows == 0) {
@@ -352,6 +380,27 @@
                         </center>
                     </td>
                 </tr>
+                <tr>
+                    <td colspan="4">
+                        <p id="anim" style="font-size: 20px;font-weight:600;padding-left: 40px;margin-top:0 0 0 0;">Contributions in an Year</p>
+                        <center>
+                            <div class="graph">
+                                <ul class="months">
+                                </ul>
+                                <ul class="days">
+                                    <li>Sun</li>
+                                    <li>Mon</li>
+                                    <li>Tue</li>
+                                    <li>Wed</li>
+                                    <li>Thu</li>
+                                    <li>Fri</li>
+                                    <li>Sat</li>
+                                </ul>
+                                <ul class="squares"></ul>
+                            </div>
+                        </center>
+                    </td>
+                </tr>
             </table>
             </td>
             <tr>
@@ -359,5 +408,71 @@
         </div>
     </div>
 </body>
+<script>
+    const squares = document.querySelector(".squares");
+    const months = document.querySelector(".months");
+
+    // Get the current month (0-indexed)
+    const currentMonth = new Date().getMonth();
+    const monthNames = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    ];
+
+    // Define sample data for contributions (0, 1, 2, or 3)
+    const sampleData = [
+        // January
+        0, 1, 2, 3, 2, 1, 0, 1, 2, 3, 4, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4,
+        // February
+        5, 6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6, 7, 6,
+        // March
+        5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8,
+        // April
+        9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2,
+        // May
+        3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4,
+        // June
+        3, 2, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6, 7,
+        // July
+        8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0,
+        // August
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7,
+        // September
+        6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4,
+        // October
+        5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3,
+        // November
+        2, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8,
+        // December
+        9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2
+    ];
+
+    // Populate the months list starting from the current month
+    for (let i = 1; i < 13; i++) {
+        const monthIndex = (currentMonth + i) % 12;
+        months.insertAdjacentHTML(
+            "beforeend",
+            `<li>${monthNames[monthIndex]}</li>`
+        );
+    }
+
+    // Populate the squares with sample data
+    sampleData.forEach(level => {
+        squares.insertAdjacentHTML(
+            "beforeend",
+            `<li data-level="${level}"></li>`
+        );
+    });
+</script>
 
 </html>
