b51708c2a268f4b82ebbaca18ed8b91c2e965638
Theternos <kavin.apm2003@gmail.com>
2023-10-02 17:27:43 +0530

    DB updated

--- SQL_Database_PAeS.sql
+++ SQL_Database_PAeS.sql
@@ -20,7 +20,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `peas`    -    Patient Enagement and Service
 --
-CREATE DATABASE 'peas'    
+CREATE DATABASE `peas`;    
 USE `peas`;
 
 -- --------------------------------------------------------
